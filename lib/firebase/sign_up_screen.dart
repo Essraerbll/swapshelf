f0ec6a9eb48a32e0100b7448d3b1df817b3e7b92
esrae <esraerbill652@gmail.com>
2024-12-05 22:18:16 +0300

    updated

--- lib/firebase/sign_up_screen.dart
+++ lib/firebase/sign_up_screen.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../login_screen.dart';
 import '../widgets/custom_background.dart';
-import 'package:swapshelfproje/phone_number_field.dart';
 
 class SignUpScreen extends StatefulWidget {
   @override
@@ -17,7 +17,34 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _jobController = TextEditingController(); // Job için eklenen controller
   bool _isLoading = false;
+  DateTime? _dateOfBirth;
+
+  final List<String> _jobs = [
+    'Software Developer',
+    'Designer',
+    'Teacher',
+    'Doctor',
+    'Engineer',
+    'Manager',
+    'Student',
+    'Lawyer',
+    'Nurse',
+    'Accountant',
+    'Architect',
+    'Data Scientist',
+    'Consultant',
+    'Writer',
+    'Artist',
+    'Photographer',
+    'Business Analyst',
+    'Chef',
+    'Electrician',
+    'Pilot',
+    'Plumber',
+    'Other',
+  ];
 
   Future<void> saveUserData(User user) async {
     try {
@@ -25,6 +52,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
         'email': user.email,
         'name': _nameController.text,
         'phone': _phoneController.text,
+        'job': _jobController.text,
+        'dob': _dateOfBirth,
         'createdAt': FieldValue.serverTimestamp(),
       });
     } catch (e) {
@@ -67,6 +96,19 @@ class _SignUpScreenState extends State<SignUpScreen> {
     }
   }
 
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != _dateOfBirth)
+      setState(() {
+        _dateOfBirth = picked;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -102,9 +144,13 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       labelStyle: TextStyle(color: Colors.white),
                       filled: true,
                       fillColor: Colors.transparent,
-                      border: OutlineInputBorder(
+                      enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide(color: Colors.white),
+                        borderSide: BorderSide(color: Colors.white, width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.white, width: 2),
                       ),
                     ),
                     style: TextStyle(color: Colors.white),
@@ -118,9 +164,14 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       labelStyle: TextStyle(color: Colors.white),
                       filled: true,
                       fillColor: Colors.transparent,
-                      border: OutlineInputBorder(
+                      prefixIcon: Icon(Icons.email, color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide(color: Colors.white),
+                        borderSide: BorderSide(color: Colors.white, width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.white, width: 2),
                       ),
                     ),
                     keyboardType: TextInputType.emailAddress,
@@ -135,20 +186,144 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       labelStyle: TextStyle(color: Colors.white),
                       filled: true,
                       fillColor: Colors.transparent,
-                      border: OutlineInputBorder(
+                      prefixIcon: Icon(Icons.lock, color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide(color: Colors.white),
+                        borderSide: BorderSide(color: Colors.white, width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.white, width: 2),
                       ),
                     ),
                     obscureText: true,
                     style: TextStyle(color: Colors.white),
                   ),
                   SizedBox(height: 10),
-                  // Phone Number Input
-                  PhoneNumberField(
+                  // Phone Number TextField
+                  TextField(
                     controller: _phoneController,
-                    focusNode: FocusNode(),
-                    onEditingComplete: () {},
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      LengthLimitingTextInputFormatter(11),
+                    ],
+                    decoration: InputDecoration(
+                      labelText: 'Phone Number',
+                      labelStyle: TextStyle(color: Colors.white),
+                      filled: true,
+                      fillColor: Colors.transparent,
+                      prefixIcon: Icon(Icons.phone, color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.white, width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.white, width: 2),
+                      ),
+                    ),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 10),
+                  // Date of Birth Picker
+                  GestureDetector(
+                    onTap: () => _selectDate(context),
+                    child: AbsorbPointer(
+                      child: TextField(
+                        controller: TextEditingController(
+                          text: _dateOfBirth == null
+                              ? ''
+                              : '${_dateOfBirth!.day}/${_dateOfBirth!.month}/${_dateOfBirth!.year}',
+                        ),
+                        decoration: InputDecoration(
+                          labelText: 'Date of Birth',
+                          labelStyle: TextStyle(color: Colors.white),
+                          filled: true,
+                          fillColor: Colors.transparent,
+                          prefixIcon: Icon(Icons.calendar_today, color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.white, width: 1),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.white, width: 2),
+                          ),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  // Job Autocomplete
+                  Autocomplete<String>(
+                    optionsBuilder: (TextEditingValue textEditingValue) {
+                      if (textEditingValue.text.isEmpty) {
+                        return const Iterable<String>.empty();
+                      }
+                      return _jobs.where((String option) {
+                        return option
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase());
+                      });
+                    },
+                    onSelected: (String selection) {
+                      _jobController.text = selection;
+                    },
+                    fieldViewBuilder: (BuildContext context,
+                        TextEditingController textEditingController,
+                        FocusNode focusNode,
+                        VoidCallback onFieldSubmitted) {
+                      _jobController.text = textEditingController.text;
+                      return TextField(
+                        controller: textEditingController,
+                        focusNode: focusNode,
+                        decoration: InputDecoration(
+                          labelText: 'Job',
+                          labelStyle: TextStyle(color: Colors.white),
+                          prefixIcon: Icon(Icons.work, color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.white, width: 1),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.white, width: 2),
+                          ),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                      );
+                    },
+                    optionsViewBuilder: (BuildContext context,
+                        AutocompleteOnSelected<String> onSelected,
+                        Iterable<String> options) {
+                      return Align(
+                        alignment: Alignment.topLeft,
+                        child: Material(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width - 32,
+                            color: Colors.blueAccent,
+                            child: ListView.builder(
+                              padding: EdgeInsets.all(8.0),
+                              itemCount: options.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                final String option = options.elementAt(index);
+                                return GestureDetector(
+                                  onTap: () {
+                                    onSelected(option);
+                                  },
+                                  child: ListTile(
+                                    title: Text(option,
+                                        style: TextStyle(color: Colors.white)),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                   SizedBox(height: 20),
                   // Sign Up Button
